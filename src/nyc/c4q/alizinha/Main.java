dbd094c8399a7b89c545e71774bb8a123f178b28
Allison Bojarski <alizinha@gmail.com>
2015-03-13 23:07:19 -0400

    adding navigation functionality

--- src/nyc/c4q/alizinha/Main.java
+++ src/nyc/c4q/alizinha/Main.java
@@ -1,10 +1,14 @@
 
 package nyc.c4q.alizinha;
 
+import sun.plugin.cache.CacheUpdateHelper;
+
+import java.util.Scanner;
+
 /**
  * Created by c4q-Allison on 3/13/15.
  */
-public class Main {g
+public class Main {
 
     public static boolean drawMap(int xCoord, int yCoord, String direction) {
         final int WIDTH = 14;
@@ -63,7 +67,85 @@ public class Main {g
 
 
     public static void main(String[] args) {
-        System.out.println(drawMap(3, 1, "left"));
+
+
+        int currentXposition=1;
+        int currentYposition=6;
+        String currentDirection = "up";
+        String convertedCommand = "";
+
+        String command = "";
+
+
+        drawMap(1, 6, currentDirection);
+
+        while (!command.equals("quit")){
+            System.out.println("Where do you want to go? (Type the 'e' key to go up, the 'x' key to go down, the 'd' key to go right, the 's' key to go left.)");
+            Scanner input = new Scanner(System.in);
+            command = input.next();
+
+            if (!command.equals("e") && !command.equals("x") && !command.equals("d") && !command.equals("s")){
+                System.out.println("invalid command");
+                continue;
+
+            }
+
+            System.out.println("Command is good.");
+
+            if(command.equals("e")) {
+                convertedCommand = "up";
+            }else if (command.equals("x")){
+                convertedCommand = "down";
+            }else if (command.equals("d")){
+                convertedCommand = "right";
+            } else if (command.equals("s")){
+                convertedCommand = "left";
+            }
+
+            if (!convertedCommand.equals(currentDirection)){
+                currentDirection=convertedCommand;
+                System.out.println("Changing orientation to:" + currentDirection);
+                drawMap(currentXposition,currentYposition,currentDirection);
+                continue;
+            }
+
+            if (currentDirection.equals("up")) {
+
+                if(drawMap(currentXposition,currentYposition-1,currentDirection)){
+                    currentYposition--;
+                }else{
+                    System.out.println("Invalid Move In +Y direction");
+                }
+            }
+            else if (currentDirection.equals("down")) {
+
+                if(drawMap(currentXposition,currentYposition+1,currentDirection)){
+                    currentYposition++;
+                }else{
+                    System.out.println("Invalid Move In -Y direction");
+                }
+            }
+            else if (currentDirection.equals("left")) {
+
+                if(drawMap(currentXposition-1,currentYposition,currentDirection)){
+                    currentXposition--;
+                }else{
+                    System.out.println("Invalid Move In -X direction");
+                }
+            }
+            else if (currentDirection.equals("right")) {
+
+                if(drawMap(currentXposition+1,currentYposition,currentDirection)){
+                    currentXposition++;
+                }else{
+                    System.out.println("Invalid Move In +X direction");
+                }
+            }
+
+
+        }
+
+
 
 
     }
